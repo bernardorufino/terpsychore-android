02d1cc47f9ec922ee6eb6039d999bf00aaa009ce
Bernardo Rufino <bermonruf@gmail.com>
2016-06-01 07:10:06 -0300

    Display notification

--- app/src/main/java/com/brufino/terpsychore/SpotifyBroadcastReceiver.java
+++ app/src/main/java/com/brufino/terpsychore/SpotifyBroadcastReceiver.java
@@ -1,8 +1,15 @@
 package com.brufino.terpsychore;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 public class SpotifyBroadcastReceiver extends BroadcastReceiver {
@@ -13,7 +20,6 @@ public class SpotifyBroadcastReceiver extends BroadcastReceiver {
         static final String QUEUE_CHANGED = SPOTIFY_PACKAGE + ".queuechanged";
         static final String METADATA_CHANGED = SPOTIFY_PACKAGE + ".metadatachanged";
     }
-
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -34,6 +40,7 @@ public class SpotifyBroadcastReceiver extends BroadcastReceiver {
                 Log.d("VFY", "  Album = " + trackAlbum);
                 Log.d("VFY", "  Length = " + trackLengthInSec + " s");
                 Log.d("VFY", "  Id = " + trackId);
+                generateNotification(context, trackName, "Enter Vibefy Session");
                 break;
             case BroadcastTypes.PLAYBACK_STATE_CHANGED:
                 int positionInMs = intent.getIntExtra("playbackPosition", 0);
@@ -44,5 +51,28 @@ public class SpotifyBroadcastReceiver extends BroadcastReceiver {
                 Log.d("VFY", "Queue changed (notif sent " + timeElapsedInMs + " ms ago)");
                 break;
         }
+    }
+
+    private Notification getNotification(Context context, String title, String description) {
+        Bitmap bitmap = ((BitmapDrawable) ContextCompat.getDrawable(context, R.drawable.ic_logo_circle)).getBitmap();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_logo_circle)
+                .setContentTitle(title)
+                .setContentText(description)
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setAutoCancel(true)
+                .setSound(null);
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
+        return builder.build();
+    }
+
+    private void generateNotification(Context context, String title, String description) {
+        Notification notification = getNotification(context, title, description);
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+
     }
 }
