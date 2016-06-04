c80e001c942ec362e21157f047a70450643ec7e7
Bernardo Rufino <bermonruf@gmail.com>
2016-06-04 05:04:49 -0300

    Implement player recovery behavior after activity killed

--- app/src/main/java/com/brufino/terpsychore/activities/SessionActivity.java
+++ app/src/main/java/com/brufino/terpsychore/activities/SessionActivity.java
@@ -14,11 +14,12 @@ import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.*;
 
+import java.lang.ref.WeakReference;
+
 import static com.google.common.base.Preconditions.*;
 
 /* TODO: Implement server-side authentication with access token refresh */
-public class SessionActivity extends AppCompatActivity
-        implements PlayerNotificationCallback, ConnectionStateCallback {
+public class SessionActivity extends AppCompatActivity {
 
     public static final String SESSION_ID_EXTRA_KEY = "sessionId";
     public static final String TRACK_ID_EXTRA_KEY = "trackId";
@@ -30,13 +31,20 @@ public class SessionActivity extends AppCompatActivity
     private static final String SPOTIFY_REDIRECT_URI = "vibefy://spotify/callback";
     private static final int SPOTIFY_LOGIN_REQUEST_CODE = 36175;
 
-    public static final String LIVE_TRACK_CURVE_SAVED_STATE_KEY = "liveTrackCurve";
     public static final String TRACK_CURVE_SAVED_STATE_KEY = "trackCurve";
+    public static final String CURRENT_POSITION_SAVED_STATE_KEY = "currentPosition";
+
+    // EPS for threshold of when to consider track finished
+    private static final double EPS = 0.0005;
+    private static final int GRAPH_UPDATE_INTERVAL_IN_MS = 80;
 
     private Player mSpotifyPlayer;
     private GraphTrackView vGraphTrackView;
     private TrackCurve mTrackCurve;
     private TrackCurve mLiveTrackCurve;
+    private boolean mActivityAlive;
+    private double mCurrentPosition;
+    private volatile boolean mSeekCurrentPosition = false; /* TODO: Analyse concurrency issues here */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,14 +78,15 @@ public class SessionActivity extends AppCompatActivity
                 .setFillColorTop(ContextCompat.getColor(this, R.color.graphForegroundTop))
                 .setStrokeTop(ContextCompat.getColor(this, R.color.graphStrokeTop), 6);
 
+        mActivityAlive = true;
         if (savedInstanceState != null) {
             mTrackCurve = savedInstanceState.getParcelable(TRACK_CURVE_SAVED_STATE_KEY);
             vGraphTrackView.addTrackCurve(mTrackCurve, trackCurveStyle);
-            Log.d("VFY", "graph track view = " + vGraphTrackView.hashCode());
-        } else {
-            mTrackCurve = TrackCurve.random();
+            mCurrentPosition = savedInstanceState.getDouble(CURRENT_POSITION_SAVED_STATE_KEY);
+            Log.d("VFY", "restoring from current position = " + mCurrentPosition);
+        } else {            mTrackCurve = TrackCurve.random();
             vGraphTrackView.addTrackCurve(mTrackCurve, trackCurveStyle);
-            new Handler().postDelayed(mUpdateLiveTrackCurve, 50);
+            mCurrentPosition = 0;
         }
     }
 
@@ -85,23 +94,15 @@ public class SessionActivity extends AppCompatActivity
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putParcelable(TRACK_CURVE_SAVED_STATE_KEY, mTrackCurve);
+        outState.putDouble(CURRENT_POSITION_SAVED_STATE_KEY, mCurrentPosition);
     }
 
-    private double mX = -0.005;
-
-    /* TODO:     We have a leakage here, observe the hash codes... */
-    private Runnable mUpdateLiveTrackCurve = new Runnable() {
-        @Override
-        public void run() {
-            mX = Math.min(1, mX + 0.005);
-            mTrackCurve.seekPosition(mX);
-            vGraphTrackView.postInvalidate();
-            Log.d("VFY", "runnable: graph track view = " + vGraphTrackView.hashCode());
-            if (mX < 1) {
-                new Handler().postDelayed(mUpdateLiveTrackCurve, 80);
-            }
-        }
-    };
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+        mActivityAlive = false;
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -110,22 +111,71 @@ public class SessionActivity extends AppCompatActivity
         switch (requestCode) {
             case SPOTIFY_LOGIN_REQUEST_CODE:
                 AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-                checkState(response.getType() == AuthenticationResponse.Type.TOKEN);
-                // int expiresIn = response.getExpiresIn();
-                // Log.d("VFY", "access token = " + response.getAccessToken());
-                // Log.d("VFY", "access token expires in " + expiresIn + " s");
-                Config playerConfig = new Config(this, response.getAccessToken(), SPOTIFY_CLIENT_ID);
-                Spotify.getPlayer(playerConfig, this, mSpotifyPlayerInitializationObserver);
+                if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                    // int expiresIn = response.getExpiresIn();
+                    // Log.d("VFY", "access token = " + response.getAccessToken());
+                    // Log.d("VFY", "access token expires in " + expiresIn + " s");
+                    Config playerConfig = new Config(this, response.getAccessToken(), SPOTIFY_CLIENT_ID);
+                    Spotify.getPlayer(playerConfig, this, mSpotifyPlayerInitializationObserver);
+                } else {
+                    Log.e("VFY", "Error! returned response type was " + response.getType());
+                }
         }
     }
 
+    private PlayerNotificationCallback mPlayerNotificationCallback = new PlayerNotificationCallback() {
+        @Override
+        public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+            // Apparently we can only reliably seekToPosition() after an AUDIO_FLUSH event
+            // See https://github.com/spotify/android-sdk/issues/12
+            if (mSeekCurrentPosition && eventType == EventType.AUDIO_FLUSH && playerState.durationInMs > 0) {
+                Log.d("VFY", "mCurrentPosition = " + mCurrentPosition);
+                int position = (int) (mCurrentPosition * playerState.durationInMs);
+                Log.d("VFY", "seeking position " + position + " ms");
+                mSpotifyPlayer.seekToPosition(position);
+                mSeekCurrentPosition = false;
+            }
+            Log.d("VFY", "onPlaybackEvent(): " + eventType.name());
+        }
+        @Override
+        public void onPlaybackError(ErrorType errorType, String s) {
+            Log.d("VFY", "onPlaybackError(): " + errorType.name());
+        }
+    };
+
+    private ConnectionStateCallback mConnectionStateCallback = new ConnectionStateCallback() {
+        @Override
+        public void onLoggedIn() {
+            Log.d("VFY", "onLoggedIn()");
+        }
+        @Override
+        public void onLoggedOut() {
+            Log.d("VFY", "onLoggedOut()");
+        }
+        @Override
+        public void onLoginFailed(Throwable throwable) {
+            Log.d("VFY", "onLoginFailed()");
+        }
+        @Override
+        public void onTemporaryError() {
+            Log.d("VFY", "onTemporaryError()");
+        }
+        @Override
+        public void onConnectionMessage(String message) {
+            Log.d("VFY", "onConnectionMessage(): message = " + message);
+        }
+    };
+
     private Player.InitializationObserver mSpotifyPlayerInitializationObserver = new Player.InitializationObserver() {
+
         @Override
         public void onInitialized(Player player) {
             mSpotifyPlayer = player;
-            mSpotifyPlayer.addConnectionStateCallback(SessionActivity.this);
-            mSpotifyPlayer.addPlayerNotificationCallback(SessionActivity.this);
-            //mSpotifyPlayer.play("spotify:track:5CKAVRV6J8sWQBCmnYICZD");
+            mSpotifyPlayer.addConnectionStateCallback(mConnectionStateCallback);
+            mSpotifyPlayer.addPlayerNotificationCallback(mPlayerNotificationCallback);
+            mSpotifyPlayer.play("spotify:track:5CKAVRV6J8sWQBCmnYICZD");
+            mSeekCurrentPosition = (mCurrentPosition > 0);
+            new Handler().post(new UpdateLiveTrackCurve(SessionActivity.this));
         }
 
         @Override
@@ -135,45 +185,38 @@ public class SessionActivity extends AppCompatActivity
         }
     };
 
-    @Override
-    public void onLoggedIn() {
-        Log.d("VFY", "onLoggedIn()");
-    }
 
-    @Override
-    public void onLoggedOut() {
-        Log.d("VFY", "onLoggedOut()");
-    }
+    private static class UpdateLiveTrackCurve implements Runnable, PlayerStateCallback {
 
-    @Override
-    public void onLoginFailed(Throwable throwable) {
-        Log.d("VFY", "onLoginFailed()");
-    }
+        // Prevent leakage if it were otherwise used with non-static inner class
+        private WeakReference<SessionActivity> mActivityRef;
 
-    @Override
-    public void onTemporaryError() {
-        Log.d("VFY", "onTemporaryError()");
-    }
+        public UpdateLiveTrackCurve(SessionActivity activity) {
+            mActivityRef = new WeakReference<>(activity);
+        }
 
-    @Override
-    public void onConnectionMessage(String message) {
-        Log.d("VFY", "onConnectionMessage(): message = " + message);
-    }
+        @Override
+        public void run() {
+            SessionActivity activity = mActivityRef.get();
+            // Make sure cycle doesn't continue if activity was destroyed
+            if (activity != null && activity.mActivityAlive) {
+                checkNotNull(activity.mSpotifyPlayer, "mSpotifyPlayer is null but shouldn't");
+                activity.mSpotifyPlayer.getPlayerState(this);
+            }
+        }
 
+        @Override
+        public void onPlayerState(PlayerState playerState) {
+            SessionActivity activity = mActivityRef.get();
+            if (activity != null && activity.mActivityAlive) {
 
-    @Override
-    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
-        Log.d("VFY", "onPlaybackEvent(): " + eventType.name());
-    }
-
-    @Override
-    public void onPlaybackError(ErrorType errorType, String s) {
-        Log.d("VFY", "onPlaybackError(): " + errorType.name());
-    }
-
-    @Override
-    protected void onDestroy() {
-        Spotify.destroyPlayer(this);
-        super.onDestroy();
+                if (playerState.durationInMs > 0 && !activity.mSeekCurrentPosition) {
+                    activity.mCurrentPosition = (double) playerState.positionInMs / playerState.durationInMs;
+                    activity.mTrackCurve.seekPosition(activity.mCurrentPosition);
+                    activity.vGraphTrackView.postInvalidate();
+                }
+                new Handler().postDelayed(this, GRAPH_UPDATE_INTERVAL_IN_MS);
+            }
+        }
     }
 }
